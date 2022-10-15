703ffbf623f4f84d82624470334ebfb2956a8399
Sujitra Chantarasmee <sujitrac@Sujitras-MacBook-Pro.local>
2022-10-15 16:03:17 -0700

    updated with further studies

--- project-tracker.sql
+++ project-tracker.sql
@@ -76,11 +76,29 @@ ALTER TABLE public.projects OWNER TO sujitrac;
 CREATE TABLE public.students (
     github character varying(30) NOT NULL,
     first_name character varying(30),
-    last_name character varying(30)
+    last_name character varying(30),
+    cohort character varying(20)
 );
 
 
 ALTER TABLE public.students OWNER TO sujitrac;
+
+--
+-- Name: report_card_view; Type: VIEW; Schema: public; Owner: sujitrac
+--
+
+CREATE VIEW public.report_card_view AS
+ SELECT students.first_name,
+    students.last_name,
+    projects.title,
+    projects.max_grade,
+    grades.grade
+   FROM ((public.students
+     JOIN public.grades ON (((students.github)::text = (grades.student_github)::text)))
+     JOIN public.projects ON (((projects.title)::text = (grades.project_title)::text)));
+
+
+ALTER TABLE public.report_card_view OWNER TO sujitrac;
 
 --
 -- Name: grades id; Type: DEFAULT; Schema: public; Owner: sujitrac
@@ -118,9 +136,9 @@ Sharkword	Guess the right word	25
 -- Data for Name: students; Type: TABLE DATA; Schema: public; Owner: sujitrac
 --
 
-COPY public.students (github, first_name, last_name) FROM stdin;
-jhacks	Jane	Hacker
-sdevelops	Sarah	Developer
+COPY public.students (github, first_name, last_name, cohort) FROM stdin;
+jhacks	Jane	Hacker	\N
+sdevelops	Sarah	Developer	\N
 \.
 
 
